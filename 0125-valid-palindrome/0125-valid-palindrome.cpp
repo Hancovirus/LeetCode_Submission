872acd1d994e6516016cf77097b8381d08a1171b
TrầnTuấnPhong <125468313+Hancovirus@users.noreply.github.com>
2024-01-04 18:17:55 +0700

    Time: 0 ms (100.00%), Space: 7.6 MB (61.79%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+#include <cctype>
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        while (j > i) {
+            while (!isalnum(s[i]) && i < j) {
+                i++;
+            }
+
+            while (!isalnum(s[j]) && i < j) {
+                j--;
+            }
+
+            if (tolower(s[i]) != tolower(s[j])) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
